12357bc93161b4673c3e7ade24f55626cb78a8c5
João Gabriel Z.B <71906858+JoaoGabrielBertolucci@users.noreply.github.com>
2023-06-20 08:58:29 -0300

    criação do projeto

---
+++ empresas/src/test/java/empresas/com/empresas/EmpresasApplicationTests.java
@@ -0,0 +1,13 @@
+package empresas.com.empresas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
